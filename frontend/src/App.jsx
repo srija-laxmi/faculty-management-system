8f45f56dca964c8db75bc933b2310de4ae8633c4
Srija Laxmi <thesrijalaxmi@gmail.com>
2026-07-23 20:48:47 +0530

    Create frontend architecture

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-950">
+      <h1 className="text-5xl font-bold text-violet-500">
+        Faculty Management System
+      </h1>
+    </div>
+  );
+}
